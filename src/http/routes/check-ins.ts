4f8473893349a36f2626b54ffe5c3c964b4474b3
Guilherme <gui.barros.js@gmail.com>
2023-07-30 03:36:30 -0300

    Refresh Token & RBAC - Autorização por cargos (RBAC)

--- src/http/routes/check-ins.ts
+++ src/http/routes/check-ins.ts
@@ -5,6 +5,7 @@ import { history } from '../controllers/check-ins/history'
 import { metrics } from '../controllers/check-ins/metrics'
 import { validate } from '../controllers/check-ins/validate'
 import { verifyJWT } from '../middlewares/verify-jwt'
+import { verifyUserRole } from '../middlewares/verify-user-role'
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
@@ -12,5 +13,9 @@ export async function checkInsRoutes(app: FastifyInstance) {
   app.get('/check-ins/history', history)
   app.get('/check-ins/metrics', metrics)
   app.post('/gyms/:gymId/check-ins', create)
-  app.patch('/check-ins/:checkInId/validate', validate)
+  app.patch(
+    '/check-ins/:checkInId/validate',
+    { onRequest: [verifyUserRole('ADMIN')] },
+    validate,
+  )
 }
--- src/http/routes/gyms.ts
+++ src/http/routes/gyms.ts
@@ -5,11 +5,12 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { create } from '../controllers/gyms/create'
 import { nearby } from '../controllers/gyms/nearby'
 import { search } from '../controllers/gyms/search'
+import { verifyUserRole } from '../middlewares/verify-user-role'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearby)
-  app.post('/gyms', create)
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 }
