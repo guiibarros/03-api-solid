c0e4c8446e75f0192c2e3b5437203fc5bb87b5df
Guilherme <gui.barros.js@gmail.com>
2023-07-30 01:29:45 -0300

    Refresh Token & RBAC - Estratégia de refresh token

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -32,9 +32,26 @@ export async function authenticate(
       },
     )
 
-    return reply.send({
-      token,
-    })
+    const refreshToken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d',
+        },
+      },
+    )
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .send({
+        token,
+      })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(401).send({
