f192c73a6c74eab0fa84a7de5205aa7b30a306c1
Guilherme <gui.barros.js@gmail.com>
2023-07-30 02:28:21 -0300

    Refresh Token & RBAC - Implementação do refresh token

--- src/http/routes/users.ts
+++ src/http/routes/users.ts
@@ -4,11 +4,13 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 import { authenticate } from '../controllers/users/authenticate'
 import { profile } from '../controllers/users/profile'
+import { refresh } from '../controllers/users/refresh'
 import { register } from '../controllers/users/register'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+  app.patch('/token/refresh', refresh)
 
   // Authenticated routes
   app.get('/me', { onRequest: [verifyJWT] }, profile)
